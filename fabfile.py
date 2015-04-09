5f286928ad859d0eb9d5d9a0845e74c331db2c49
Evren Esat Özkan <mail@my.blog.uri>
2015-04-09 13:58:08 +0300

    refactored RiakTest class, added PyPy support to improve on the fly random data creation added gevent support to improve threaded network performance added a fabfile to execute tests on the remote machine.

---
+++ fabfile.py
@@ -0,0 +1,54 @@
+# -*-  coding: utf-8 -*-
+"""
+run test_methods.py on remote
+'fab test' for CPython env
+'fab test:pypy' for PyPy
+"""
+# Copyright (C) 2015 ZetaOps Inc.
+#
+# This file is licensed under the GNU General Public License v3
+# (GPLv3).  See LICENSE.txt for details.
+from __future__ import with_statement
+import sys
+from fabric.api import env, run, cd, prefix
+from fabric.contrib.project import rsync_project
+from contextlib import contextmanager as _contextmanager
+
+# env.hosts = ['servername']
+# env.keyfile = ['$HOME/.ssh/deploy_rsa']
+env.user = 'kunthar'
+env.hosts = ['62.210.245.199']
+env.port = '99'
+
+if 'pypy' in ''.join(sys.argv):
+    env.directory = '/home/kunthar/test-riak-pypy'
+    env.activate = 'source /home/kunthar/.virtualenvs/pypy/bin/activate'
+else:
+    env.directory = '/home/kunthar/test-riak'
+    env.activate = 'source /home/kunthar/.virtualenvs/test-riak/bin/activate'
+
+
+@_contextmanager
+def virtualenv():
+    with cd(env.directory):
+        with prefix(env.activate):
+            yield
+
+
+def sync():
+    """
+    syncs the files with test machine (to home dir. by default)
+    """
+    rsync_project(env.directory, local_dir=".", default_opts='-pthrz',
+                  exclude="*.log *.json fabfile *.pyc *~ .idea .git *.png".split(' '),
+                  )
+
+
+def _test():
+    with virtualenv():
+        run("python test_methods.py")
+
+
+def test(foo=None):
+    sync()
+    _test()
--- test_methods.py
+++ test_methods.py
@@ -1,5 +1,6 @@
 # -*-  coding: utf-8 -*-
 """
+Mass data storage test case for Riak
 """
 
 # Copyright (C) 2015 ZetaOps Inc.
@@ -9,57 +10,87 @@
 from thread import start_new_thread
 import time
 
-# from connection import client
+import sys
+from gevent import monkey
+monkey.patch_all()
+
 import threading
+import uuid
 from schemas import make_student_data
 import riak
+
 
 DEFAULT_BUCKET_NAME = 'my_bucket'
 
 
 class RiakTest(object):
-    def __init__(self, test_method, concurrency=1, bucket_name=None):
+    def __init__(self, workers=1, bucket_name=None, total_records=100):
 
         self.BUCKET_NAME = bucket_name or DEFAULT_BUCKET_NAME
-        self.concurrency = concurrency
-        self.save_method = getattr(self, test_method)
-        self.bucket = self.get_bucket()
+        self.workers = workers
 
-    def get_bucket(self):
-        return riak.RiakClient(protocol='pbc', host='62.210.245.199', pb_port='8087').bucket(self.BUCKET_NAME)
+        self.record_per_worker = total_records / self.workers
+        self.total_records = total_records
+        self.student = make_student_data()
+        self.client = riak.RiakClient
 
-    def save_students(self, bucket):
+
+    def setup_client(self, host, port, typ='pbc'):
+        self.client = riak.RiakClient(protocol='pbc', host=host, pb_port=port) \
+            if typ == 'pbc' else \
+            riak.RiakClient(protocol='http', host=host, http_port=port)
+
+    def save_students(self):
+        # sleep(0.1)
         student = make_student_data()
-        std_obj = bucket.new(student['identity_information']['tc_no'], student)
-        std_obj.store()
+        # sleep(0.1)
+        self.client.bucket(self.BUCKET_NAME).new(student['identity_information']['tc_no'], student).store()
+
+    def save_same_student(self):
+        self.client.bucket(self.BUCKET_NAME).new(str(uuid.uuid1()), self.student).store()
+
 
     def range_save(self):
-        bucket = self.get_bucket()
-        for i in range(100):
-            self.save_students(bucket)
-            # self.save_students()
+        for i in range(self.record_per_worker):
+            self.test_method()
 
     def run(self):
-        threads = [threading.Thread(target=self.range_save, args=()) for i in range(self.concurrency)]
+        threads = [threading.Thread(target=self.range_save, args=()) for i in range(self.workers)]
         for t in threads:
             t.start()
         for t in threads:
             t.join()
 
-    def count_keys(self):
-        return sum([len(x) for x in self.bucket.stream_keys()])
 
-    def start_test(self):
+    def count_keys(self):
+        return sum([len(key_list) for key_list in self.client.bucket(self.BUCKET_NAME).stream_keys()])
+
+    def start_test(self, method):
+        self.test_method = getattr(self, method)
         existing_record_count = self.count_keys()
         start_time = time.time()
         self.run()
         end_time = time.time()
-        new_record_count = self.count_keys()
-        total_new_record_count = new_record_count - existing_record_count
         elapsed_time = round(end_time - start_time, 1)
-        print("%s records saved in %s secs." % (total_new_record_count, elapsed_time))
+        report = {'elapsed': elapsed_time, 'new_count': self.count_keys(),
+                  'workers': self.workers, 'record_per_worker': self.record_per_worker,
+                  'test_method': method, 'sys_info': sys.version}
+        report['total_new'] = report['new_count'] - existing_record_count
+        report['per_sec'] = round(report['total_new'] / elapsed_time)
+        report['per_sec_per_worker'] = round(report['total_new'] / elapsed_time / self.workers)
+        print("\n\n:::REPORT:::::::::::::::::::::\n\n"
+              "{sys_info}\n\n"
+              "{test_method} method running with the following configurations:\n\n"
+              "{total_new} records saved in {elapsed} secs.\n"
+              "Total record count: {new_count}\n"
+              "{workers} worker run with {record_per_worker} jobs for each.\n"
+              "{per_sec} records stored per sec.\n"
+              "{per_sec_per_worker} record stored by each worker per second\n\n"
+              ":::::::::::::::::::::::::::::::\n\n".format(**report))
 
 
 if __name__ == '__main__':
-    rt = RiakTest('save_students', concurrency=2, bucket_name='student')
-    rt.start_test()
+    rt = RiakTest(workers=5, bucket_name='student', total_records=10000)
+    rt.setup_client(host='10.91.5.26', port=8087, typ='pbc')  # z5 public ip: 62.210.245.199 http port: 8098
+    rt.start_test(method='save_students')
+    # rt.start_test(method='save_same_student')
