52df836d7d1c2c9ba6ff714a4c66733368603d87
Evren Esat Özkan <sleytr@gmail.com>
2015-06-04 15:21:49 +0300

    cleanup

--- pyoko/db/solr_schema_fields.py
+++
@@ -1,8 +0,0 @@
-SOLR_FIELDS = {
-'string': '<field type="string" name="{name}"  indexed="{index}" stored="{store}" multiValued="{multi}" />',
-'text': '<field type="string"  name="{name}"  indexed="{index}" stored="{store}" multiValued="{multi}" />',
-'float': '<field type="float"  name="{name}"  indexed="{index}" stored="{store}" multiValued="{multi}" />',
-'int': '<field type="int" name="{name}"  indexed="{index}" stored="{store}" multiValued="{multi}" />',
-'bool': '<field type="boolean" name="{name}"  indexed="{index}" stored="{store}" multiValued="{multi}" />',
-'local': '<field type="text_{type}" name="{name}"  indexed="{index}" stored="{store}" multiValued="{multi}" />'
-}
