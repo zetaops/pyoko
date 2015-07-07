6b1bcaf1b230c8b21581b82b8031d3635ff45ead
Evren Esat Özkan <sleytr@gmail.com>
2015-07-07 10:59:59 +0300

    added custom form support with pyoko.form.Form added model binding support for received form data

--- pyoko/form.py
+++ pyoko/form.py
@@ -7,29 +7,70 @@ this module holds classes that responsible for form generation both from models 
 #
 # This file is licensed under the GNU General Public License v3
 # (GPLv3).  See LICENSE.txt for details.
-from pyoko.lib.utils import un_camel
+from pyoko.field import BaseField
+from pyoko.lib.utils import un_camel, to_camel
+
 
 class ModelForm(object):
     def __init__(self, model, **kwargs):
+        """
+        keyword arguments:
+            base_fields = True
+            nodes = True
+            linked_models = True
+            list_nodes = False
+            types = {'field_name':'type', 'password':'password'} modify type of fields.
+        :param pyoko.Model model: A pyoko model instance, may be empty or full.
+        :param dict kwargs: configuration options
+        """
         self.model = model
         if not kwargs or 'all' in kwargs:
             kwargs = {'base_fields': 1, 'nodes': 1, 'linked_models': 1}
             if 'all' in kwargs:
                 kwargs['list_nodes'] = 1
         self.config = kwargs
+        self.customize_types = kwargs.get('types', {})
         self.title = kwargs.get('title', self.model.__class__.__name__)
 
+    def deserialize(self, data):
+        """
+        returns the model loaded with received form data.
+        :param dict data: received form data from client
+        """
+        #FIXME: we should investigate and integrate necessary security precautions on received data
+        #TODO: add listnode support when format of incoming data for listnodes defined
+        proccessed_data = {}
+        for key, val in data.items():
+            if '.' in key:
+                keys = key.split('.')
+                if keys[0] not in proccessed_data:
+                    proccessed_data[keys[0]] = {}
+                proccessed_data[keys[0]][keys[1]] = val
+            else:
+                proccessed_data[key] = val
+        self.model._load_data(proccessed_data)
+        return self.model
+
     def _serialize(self):
+        """
+        :return: serialized model fields
+        """
+        # TODO: to return in consistent order we should iterate over sorted list of keys
         while 1:
             if 'base_fields' in self.config:
                 for name, field in self.model._fields.items():
                     if name in ['deleted', 'timestamp']: continue
+                    value = self.model._field_values.get(name, '')
+                    if value:
+                        default = None
+                    else:
+                        default = field.default() if callable(field.default) else field.default
                     yield {'name': name,
-                           'type': field.solr_type,
-                           'value': self.model._field_values.get(name, ''),
+                           'type': self.customize_types.get(name, field.solr_type),
+                           'value': value,
                            'required': field.required,
                            'title': field.title,
-                           'default': field.default() if callable(field.default) else field.default,
+                           'default': default,
                            'section': 'main',
                            'storage': 'main',
                            }
@@ -42,7 +83,7 @@ class ModelForm(object):
                         for name, field in instance_node._fields.items():
                             if name in ['deleted', 'timestamp']: continue
                             yield {'name': "%s.%s" % (un_camel(node_name), name),
-                                   'type': field.solr_type,
+                                   'type': self.customize_types.get(name, field.solr_type),
                                    'title': field.title,
                                    'value': self.model._field_values.get(name, ''),
                                    'required': field.required,
@@ -64,3 +105,29 @@ class ModelForm(object):
             break
 
 
+
+class Form(ModelForm):
+    """
+    base class for a custom form with pyoko.fields
+    """
+    def __init__(self, **kwargs):
+        self._nodes = {}
+        self._fields = {}
+        self._linked_models = {}
+        self._field_values = {}
+        for key, val in self.__class__.__dict__.items():
+            if isinstance(val, BaseField):
+                val.name = key
+                self._fields[key] = val
+        super(Form, self).__init__(self, **kwargs)
+
+
+    def _load_data(self, data):
+        """
+        fills form with data
+        :param dict data:
+        :return: self
+        """
+        for name in self._fields:
+            setattr(self, name, data.get(name))
+        return self
--- tests/test_form_generation.py
+++ tests/test_form_generation.py
@@ -9,7 +9,7 @@
 import datetime
 from pprint import pprint
 from time import sleep
-from pyoko.form import ModelForm
+from pyoko.form import ModelForm, Form
 from tests.data.test_data import data
 
 from tests.models import *
@@ -26,7 +26,7 @@ raw_form_output = [
     {'storage': 'main', 'default': None, 'name': 'bio', 'section': 'main', 'required': True,
      'value': 'Lorem impsum dolar sit amet falan filan', 'type': 'text_general',
      'title': 'Biography'},
-    {'storage': 'main', 'default': '', 'name': 'join_date', 'section': 'main',
+    {'storage': 'main', 'default': None, 'name': 'join_date', 'section': 'main',
      'required': True, 'value': datetime.date(2015, 5, 16), 'type': 'date', 'title': 'Join Date'},
     {'storage': 'main', 'default': None, 'name': 'name', 'section': 'main', 'required': True,
      'value': 'Jack', 'type': 'text_tr', 'title': 'First Name'},
@@ -37,14 +37,43 @@ raw_form_output = [
     {'storage': 'main', 'default': None, 'name': 'surname', 'section': 'main', 'required': True,
      'value': 'Black', 'type': 'text_tr', 'title': 'Last Name'}]
 
+received_data = {
+    'auth_info.email': 'duuper@suup.com',
+    'auth_info.password': '1111',
+    'auth_info.username': 'poser',
+    'bio': "You think water moves fast? You should see ice. It moves like it has a mind. "
+           "Like it knows it killed the world once and got a taste for murder. "
+           "After the avalanche, it took us a week to climb out.",
+    'join_date': datetime.date(2015, 5, 16),
+    'name': 'Samuel',
+    'archived': False,
+    'deleted': False,
+    'number': '20300344',
+    'timestamp': None,
+    'pno': '2343243433',
+    'surname': 'Jackson'}
+
+
+class LoginForm(Form):
+    username = field.String("Username")
+    password = field.String("Password")
+
+
+
+serialized_login_form = [
+    {'value': '', 'name': 'password', 'storage': 'main',
+     'default': None, 'type': 'string', 'section': 'main',
+     'required': True, 'title': 'Password'},
+    {'value': '', 'name': 'username', 'storage': 'main', 'default': None,
+     'type': 'string', 'section': 'main', 'required': True,
+     'title': 'Username'}
+]
 
 class TestModelRelations:
-    """
-    """
     cleaned_up = False
 
     @classmethod
-    def cleanup(cls):
+    def clean(cls):
         if not cls.cleaned_up:
             for model in [Student, ]:
                 model.objects._clear_bucket()
@@ -52,9 +81,29 @@ class TestModelRelations:
             cls.cleaned_up = True
 
     def test_modelform_serialize_simple(self):
-        self.cleanup()
+        self.clean()
         student = Student()
         student._load_data(data)
         student.save()
         serialized_model = sorted(ModelForm(student)._serialize(), key=lambda d: d['name'])
         assert raw_form_output == serialized_model
+
+    def test_plain_form(self):
+        serialized_model = sorted(LoginForm()._serialize(), key=lambda d: d['name'])
+        assert serialized_model == serialized_login_form
+
+    def test_plain_form_deserialize(self):
+        login_data = {'username': 'Samuel', 'password': 'seeice'}
+        model = LoginForm().deserialize(login_data)
+        assert model.password == login_data["password"]
+        assert model.username == login_data["username"]
+
+    def test_modelform_deserialize_simple(self):
+        self.clean()
+        student = ModelForm(Student()).deserialize(received_data)
+        student.save()
+        sleep(1)
+        db_student = Student.objects.filter(
+            auth_info__email=received_data['auth_info.email']).get()
+        assert db_student.AuthInfo.email == received_data['auth_info.email']
+        assert db_student.bio == received_data['bio']
