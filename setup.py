164cc9dfdf03d88b96839f466126cdc262f50fc9
Evren Kutar <furiston@gmail.com>
2015-07-23 15:53:32 +0300

    install requires satiri eklendi

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     description='Pyoko is a Django-esque lightweight ORM for Riak/Solr (aka Yokozuna)',
     author='Zetaops',
     requires=['enum34', 'six', 'riak'],
+    install_requires=['enum34', 'six', 'riak'],
     author_email='info@zetaops.io',
     url='https://github.com/zetaops/pyoko',
     packages=find_packages(exclude=['tests', 'tests.*']),
