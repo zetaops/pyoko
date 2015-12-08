008cf81cf8e90d17354c2843f5acd4b80030e4e9
Evren Esat Özkan <sleytr@gmail.com>
2015-12-08 08:20:03 +0200

    refactored each class into different files

---
+++ pyoko/listnode.py
@@ -0,0 +1,166 @@
+# -*-  coding: utf-8 -*-
+"""
+"""
+
+# Copyright (C) 2015 ZetaOps Inc.
+#
+# This file is licensed under the GNU General Public License v3
+# (GPLv3).  See LICENSE.txt for details.
+import six
+
+from .node import Node
+from .lib.utils import un_camel, un_camel_id
+
+
+class ListNode(Node):
+    """
+    Currently we disregard the ordering when updating items of a ListNode
+    """
+
+    # HASH_BY = '' # calculate __hash__ value by field defined here
+    _TYPE = 'ListNode'
+
+    def __init__(self, **kwargs):
+        self._is_item = False
+        self._from_db = False
+        self.values = []
+        self.node_stack = []
+        self._data = []
+        self.node_dict = {}
+        super(ListNode, self).__init__(**kwargs)
+
+    def _load_data(self, data, from_db=False):
+        """
+        just stores the data at self._data,
+        actual object creation done at _generate_instances()
+        """
+        self._data = data
+        self.data = data[:]
+        self._from_db = from_db
+
+    def _generate_instances(self):
+        """
+        a clone generator that will be used by __iter__ or __getitem__
+        """
+        for node in self.node_stack:
+            yield node
+        while self._data:
+            yield self._make_instance(self._data.pop(0))
+
+    def _make_instance(self, node_data):
+        """
+        create a ListNode instance from node_data
+
+        :param dict node_data:
+        :return: ListNode item
+        """
+        node_data['from_db'] = self._from_db
+        clone = self.__call__(**node_data)
+        clone.container = self
+        clone._is_item = True
+        for name in self._nodes:
+            _name = un_camel(name)
+            if _name in node_data:  # check for partial data
+                getattr(clone, name)._load_data(node_data[_name])
+        for name in self._linked_models:
+            ins = getattr(clone, name)
+            self.node_dict[ins.key] = clone
+            break  # only one linked_model can represent an item
+        return clone
+
+    def clean_value(self):
+        """
+        populates json serialization ready data for storing on riak
+        :return: [{},]
+        """
+        result = []
+        for mdl in self:
+            # mdl.processed_nodes = self.processed_nodes
+            result.append(super(ListNode, mdl).clean_value())
+        return result
+
+    def __repr__(self):
+        """
+        this works for two different object.
+        - Main ListNode object
+        - Items of the node (like instance of a class) which created on iteration of main object
+        :return:
+        """
+        if not self._is_item:
+            return [obj for obj in self[:10]].__repr__()
+        else:
+            try:
+                u = six.text_type(self)
+            except (UnicodeEncodeError, UnicodeDecodeError):
+                u = '[Bad Unicode data]'
+            return six.text_type('<%s: %s>' % (self.__class__.__name__, u))
+
+    # def __hash__(self):
+    #     if self.HASH_BY:
+    #         return hash(getattr(self, self.HASH_BY))
+
+    def add(self, **kwargs):
+        """
+        stores node data without creating an instance of it
+        this is more efficient if node instance is not required
+        :param kwargs: properties of the ListNode
+        :return: None
+        """
+        self._data.append(kwargs)
+
+    def __call__(self, **kwargs):
+        """
+        stores created instance in node_stack and returns it's reference to callee
+        :param kwargs:
+        :return:
+        """
+        kwargs['root'] = self.root
+        clone = self.__class__(**kwargs)
+        # clone.root = self.root
+        clone._is_item = True
+        clone.processed_nodes = self.root.processed_nodes
+        self.node_stack.append(clone)
+        return clone
+
+    def clear(self):
+        """
+        clear outs the list node
+        """
+        if self._is_item:
+            raise TypeError("This an item of the parent ListNode")
+        self.node_stack = []
+        self._data = []
+
+    def __contains__(self, item):
+        if self._data:
+            return any([d[un_camel_id(item.__class__.__name__)] == item.key for d in self._data])
+        else:
+            return item.key in self.node_dict
+
+    def __len__(self):
+        return len(self._data or self.node_stack)
+
+    def __getitem__(self, index):
+        return list(self._generate_instances()).__getitem__(index)
+
+    def __iter__(self):
+        return self._generate_instances()
+
+    def __setitem__(self, key, value):
+        if self._is_item:
+            raise TypeError("This an item of the parent ListNode")
+        self.node_stack[key] = value
+
+    def __delitem__(self, key):
+        if self._is_item:
+            raise TypeError("This an item of the parent ListNode")
+        self.node_stack.remove(self[key])
+
+    def remove(self):
+        """
+        remove this item from list node
+        note: you should save the parent object yourself.
+        """
+        if not self._is_item:
+            raise TypeError("A ListNode cannot be deleted")
+        self.container.node_stack.remove(self)
---
+++ pyoko/registry.py
@@ -0,0 +1,120 @@
+# -*-  coding: utf-8 -*-
+"""
+"""
+
+# Copyright (C) 2015 ZetaOps Inc.
+#
+# This file is licensed under the GNU General Public License v3
+# (GPLv3).  See LICENSE.txt for details.
+from collections import defaultdict
+
+from pyoko.lib.utils import un_camel
+
+
+class Registry(object):
+    def __init__(self):
+        self.registry = {}
+        self.lazy_models = {}
+        self.app_registry = defaultdict(dict)
+        # self.link_registry = defaultdict(list)
+
+    def register_model(self, mdl):
+        if mdl not in self.registry:
+            self.registry[mdl.__name__] = mdl
+            self.app_registry[mdl.Meta.app][mdl.__name__] = mdl
+            self._process_links_from_nodes(mdl)
+            self._process_lazy_links(mdl)
+            self._process_links(mdl)
+
+    def _process_links(self, mdl):
+        for name, vals in mdl._linked_models.items():
+            for val in vals:
+                # custom reverse name or model name if one to one
+                # or model_name_set otherwise
+                reverse_name = un_camel(
+                        val['reverse'] or mdl.__name__ + ('' if val['o2o'] else '_set'))
+                # self.link_registry[val['mdl']].append((name, mdl, reverse_name))
+                if val['o2o']:
+                    self._create_one_to_one(mdl, val['mdl'], reverse_name)
+                    val['mdl']._add_linked_model(mdl, o2o=True, field=reverse_name)
+                else:
+                    val['mdl']._add_linked_model(mdl, reverse=val['field'], field=reverse_name)
+                    self._create_one_to_many(mdl, val['mdl'], reverse_name)
+
+    def _process_lazy_links(self, target_mdl):
+        if target_mdl.__name__ in self.lazy_models:
+            for lm in self.lazy_models[target_mdl.__name__]:
+                source_mdl = self.registry[lm['from']]
+                source_mdl._add_linked_model(target_mdl, lm['o2o'], lm['field'], lm['reverse'],
+                                             lm['verbose'])
+                target_mdl._add_linked_model(source_mdl, reverse=lm['field'], field=lm['reverse'])
+                self._create_one_to_many(source_mdl, target_mdl, lm['reverse'])
+
+    def _process_links_from_nodes(self, source_mdl):
+        for node in source_mdl._nodes.values():
+            for vals in node._linked_models.values():
+                for val in vals:
+                    # self.link_registry[val['mdl']].append((val['field'],
+                    #                                        source_mdl,
+                    #                                        val['reverse']))
+                    reverse_name = un_camel(
+                            val['reverse'] or source_mdl.__name__ + ('' if val['o2o'] else '_set'))
+                    if not val['o2o']:
+                        # Role.Permisions(permission=Permission()) -->
+                        # --> Permission.role_set (or if given, custom
+                        #  reverse name)
+                        val['mdl']._add_linked_model(source_mdl, o2o=False, field=reverse_name,
+                                                     reverse=val['field'])
+                        self._create_one_to_many(source_mdl, val['mdl'], val['reverse'])
+                    else:
+                        val['mdl']._add_linked_model(source_mdl, o2o=True, field=reverse_name,
+                                                     reverse=val['field'])
+                        self._create_one_to_one(source_mdl, val['mdl'], val['reverse'])
+
+    def _create_one_to_one(self, source_mdl, target_mdl, field_name):
+        mdl_instance = source_mdl(one_to_one=True)
+        mdl_instance._is_auto_created = True
+        for instance_ref in target_mdl._instance_registry:
+            mdl = instance_ref()
+            if mdl:  # if not yet garbage collected
+                setattr(mdl, field_name, mdl_instance)
+                # target_mdl._add_linked_model(source_mdl, o2o=True, field=field_name)
+
+    def _create_one_to_many(self, source_mdl, target_mdl, listnode_name=None):
+        # other side of n-to-many should be a ListNode
+        # with our source model as the sole element
+        if not listnode_name:
+            listnode_name = '%s_set' % un_camel(source_mdl.__name__)
+        from .listnode import ListNode
+        source_instance = source_mdl()
+        source_instance._is_auto_created = True
+        # create a new class which extends ListNode
+        listnode = type(listnode_name, (ListNode,),
+                        {source_mdl.__name__: source_instance,
+                         '_is_auto_created': True})
+        listnode._add_linked_model(source_mdl, o2o=False, field=listnode_name,
+                                   reverse=source_mdl.__name__)
+        # source_mdl._add_linked_model(target_mdl, o2o=False, )
+        target_mdl._nodes[listnode_name] = listnode
+        # add just created model_set to model instances that
+        # initialized inside of another model as linked model
+        for instance_ref in target_mdl._instance_registry:
+            mdl = instance_ref()
+            if mdl:  # if not yet garbage collected
+                mdl._instantiate_node(listnode_name, listnode)
+
+    def get_base_models(self):
+        return self.registry.values()
+
+    def get_apps(self):
+        return self.app_registry.keys()
+
+    def get_models_by_apps(self):
+        return [(app_names, model_dict.values())
+                for app_names, model_dict in self.app_registry.items()]
+
+    def get_model(self, model_name):
+        return self.registry[model_name]
+
+    def get_models_of_app(self, app_name):
+        return self.app_registry[app_name].values()
