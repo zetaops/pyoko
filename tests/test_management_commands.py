494b6b52bf4c445c8885d15abad6bb07a7989d1f
Kaan Genç <kaan@kaangenc.me>
2016-09-06 12:13:50 +0300

    Add tests for data dumps
    Added tests for different types of dumps, and per model dumps.

--- tests/test_management_commands.py
+++ tests/test_management_commands.py
@@ -10,6 +10,9 @@
 import codecs
 from time import sleep
 from pyoko.manage import ManagementCommands
+from models import Person, User
+import tempfile
+import os
 
 
 def test_load_dump_data():
@@ -22,6 +25,89 @@ def test_load_dump_data():
     ManagementCommands(args=['dump_data', '--model', 'Student', '--path', path])
     with codecs.open(path, encoding='utf-8') as file:
         assert out == file.read()
+
+def test_dump_per_model():
+    # Test if the per model dumps work
+    path = tempfile.mkdtemp(prefix='pyoko_test_')
+    ManagementCommands(args=['dump_data',
+                             '--model', 'Person,User',
+                             '--path', path,
+                             '--per_model'])
+    # Make sure the dumps were created
+    person_path = os.path.join(path, 'Person.csv')
+    assert os.path.isfile(person_path)
+    user_path = os.path.join(path, 'User.csv')
+    assert os.path.isfile(user_path)
+    # Check if the dumps contain correct students
+    with codecs.open(person_path) as person_file:
+        persons_dumped = person_file.read()
+    assert Person.objects.count() > 0
+    for person in Person.objects:
+        assert person.key in persons_dumped
+    # Check if the dumps contain correct users
+    with codecs.open(user_path) as user_file:
+        users_dumped = user_file.read()
+    assert User.objects.count() > 0
+    for user in User.objects:
+        assert user.key in users_dumped
+
+
+def test_dump_json():
+    # Test if the per model dumps work
+    handle, path = tempfile.mkstemp(prefix='pyoko_test_', suffix='.json')
+    ManagementCommands(args=['dump_data',
+                             '--model', 'Person,User',
+                             '--path', path,
+                             '--type', 'json'])
+    # Check if the dump contains correct students
+    with codecs.open(path) as file_:
+        data_dumped = file_.read()
+    assert Person.objects.count() > 0
+    for person in Person.objects:
+        assert person.key in data_dumped
+    # Check if the dump contains correct users
+    assert User.objects.count() > 0
+    for user in User.objects:
+        assert user.key in data_dumped
+
+
+def test_dump_json_tree():
+    # Test if the per model dumps work
+    handle, path = tempfile.mkstemp(prefix='pyoko_test_', suffix='.json')
+    ManagementCommands(args=['dump_data',
+                             '--model', 'Person,User',
+                             '--type', 'json_tree',
+                             '--path', path])
+    # Check if the dump contains correct students
+    with codecs.open(path) as file_:
+        data_dumped = file_.read()
+    assert Person.objects.count() > 0
+    for person in Person.objects:
+        assert person.key in data_dumped
+    # Check if the dump contains correct users
+    assert User.objects.count() > 0
+    for user in User.objects:
+        assert user.key in data_dumped
+
+
+def test_dump_pretty():
+    # Test if the per model dumps work
+    handle, path = tempfile.mkstemp(prefix='pyoko_test_', suffix='.json')
+    ManagementCommands(args=['dump_data',
+                             '--model', 'Person,User',
+                             '--path', path,
+                             '--type', 'pretty'])
+    # Check if the dump contains correct students
+    with codecs.open(path) as file_:
+        data_dumped = file_.read()
+    assert Person.objects.count() > 0
+    for person in Person.objects:
+        assert person.key in data_dumped
+    # Check if the dump contains correct users
+    assert User.objects.count() > 0
+    for user in User.objects:
+        assert user.key in data_dumped
+
 
 def test_apply_solr_schema():
     # TODO: Currently only tests if it's running without giving any errors, should assert something
