ad835755ecf5b1201058cbad36ec2ba9fa6d65b3
furkanUyar <furkanuyar92@gmail.com>
2016-10-26 16:30:53 +0300

    ADD, versioning and logging mechanism and tests are completed rref #5496

--- pyoko/settings.py
+++ pyoko/settings.py
@@ -9,6 +9,8 @@ Default Settings
 import os
 
 DEFAULT_BUCKET_TYPE = os.environ.get('DEFAULT_BUCKET_TYPE', 'pyoko_models')
+VERSION_LOG_BUCKET_TYPE = os.environ.get('VERSION_LOG_BUCKET_TYPE', 'version_log')
+
 RIAK_SERVER = os.environ.get('RIAK_SERVER', 'localhost')
 RIAK_PROTOCOL = os.environ.get('RIAK_PROTOCOL', 'http')
 RIAK_PORT = os.environ.get('RIAK_PORT', 8098)
@@ -20,14 +22,14 @@ REDIS_SERVER = os.environ.get('REDIS_SERVER', '127.0.0.1:6379')
 REDIS_PASSWORD = os.environ.get('REDIS_PASSWORD', None)
 
 #: Set True to enable versioning on write-once buckets
-ENABLE_VERSIONS = os.environ.get('ENABLE_VERSIONS', 'False') == 'True'
-
+ENABLE_VERSIONS = os.environ.get('ENABLE_VERSIONS', True)
 #: Suffix for version buckets
 VERSION_SUFFIX = os.environ.get('VERSION_SUFFIX', '_version')
 
 #: Set True to enable auto-logging of all DB operations to a
 #: write-once log bucket
-ENABLE_ACTIVITY_LOGGING = os.environ.get('VERSION_SUFFIX', 'False') == 'True'
+ENABLE_ACTIVITY_LOGGING = os.environ.get('ENABLE_ACTIVITY_LOGGING', True)
 
 #: Set the name of logging bucket type and bucket name.
-ACTIVITY_LOGGING_BUCKET = os.environ.get('ACTIVITY_LOGGING_BUCKET', DEFAULT_BUCKET_TYPE + '_log')
+ACTIVITY_LOGGING_BUCKET = os.environ.get('ACTIVITY_LOGGING_BUCKET', 'log')
+VERSION_BUCKET = os.environ.get('VERSION_BUCKET', 'version')
