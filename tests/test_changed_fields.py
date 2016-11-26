069ec73ac6d27eb9ef72be2e4dd8327d9c05cf41
furkanUyar <furkanuyar92@gmail.com>
2016-11-26 16:51:51 +0200

    ADD, test is added for changed_fields and is_changed methods rref #5435

---
+++ tests/test_changed_fields.py
@@ -0,0 +1,56 @@
+# -*-  coding: utf-8 -*-
+"""
+"""
+
+# Copyright (C) 2015 ZetaOps Inc.
+#
+# This file is licensed under the GNU General Public License v3
+# (GPLv3).  See LICENSE.txt for details.
+from datetime import datetime
+from .models import Student, TimeTable, Uniques, Role, User
+
+
+class TestCase:
+    def test_changed_fields(self):
+
+        # String, Date and ListNode test
+        s = Student.objects.filter()[0]
+        name = '%s_test' % s.name
+        s.name = name
+        s.join_date = datetime.now().date()
+        s.Lectures(name='test_string')
+        assert s.is_changed('name') == s.is_changed('join_date') == s.is_changed('lectures') == True
+        assert 'name' and 'join_date' and 'lectures' in s.changed_fields()
+        assert 'pno' and 'number' not in s.changed_fields()
+
+        # Integer test
+        t = TimeTable.objects.filter()[0]
+        week_day = t.week_day + 1
+        t.week_day = week_day
+        assert t.is_changed('week_day') == True
+        assert 'week_day' in t.changed_fields()
+        assert 'lecture' and 'hours' not in t.changed_fields()
+
+        # Datetime test
+        u = Uniques.objects.filter()[0]
+        assert u.join_date != datetime.now()
+        u.join_date = datetime.now()
+        assert u.is_changed('join_date') == True
+        assert 'join_date' in u.changed_fields()
+        assert 'rel_id' and 'id' not in u.changed_fields()
+
+        # Boolean and Link test
+        r = Role.objects.filter()[0]
+        user = ''
+        for user in User.objects.filter():
+            if user != r.usr:
+                break
+        bool = r.active
+        r.active = bool
+        assert r.is_changed('active') == False
+        assert 'active' not in r.changed_fields()
+        r.active = not bool
+        r.usr = user
+        assert r.is_changed('usr_id') == r.is_changed('active') == True
+        assert 'active' and 'usr_id' in r.changed_fields()
+        r.active = bool
